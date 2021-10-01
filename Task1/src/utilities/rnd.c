aaf4f09a1d86b1b22c1eebe2937655636a895b12
NullOne01 <NullOne01@users.noreply.github.com>
2021-10-01 03:56:33 +0300

    Made function RandomString not inline.

---
+++ Task1/src/utilities/rnd.c
@@ -0,0 +1,20 @@
+//------------------------------------------------------------------------------
+// rnd.c - содержит реализации утилит, связанных со случайной генерацией.
+//------------------------------------------------------------------------------
+
+#include "rnd.h"
+#include <string.h>
+
+char *RandomString(char *str, size_t size) {
+    const char *charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if (size) {
+        --size;
+        for (size_t n = 0; n < size; n++) {
+            int key = RandomRange(0, strlen(charset) - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+    return str;
+}
+
